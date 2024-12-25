9a615e48e6f5e24f7e99afe4de4de00be580d616
flhs16 <1686684458@qq.com>
2024-12-25 23:06:35 +0800

    Site updated: 2024-12-25 23:06:34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735139020714";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735139189198";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
