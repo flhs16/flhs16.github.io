5a7a12a4b935a2b8b1930f1d508b75fc8b4ae4ba
flhs16 <1686684458@qq.com>
2025-01-03 20:49:21 +0800

    Site updated: 2025-01-03 20:49:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735286262560";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735908553998";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
